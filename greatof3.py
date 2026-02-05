b36839b5d987d4f858eccf47b33fa634f170d60a
ABHIJITH GOWDA D R <abhijithgowdadrbca25@rvu.edu.in>
2026-02-05 10:19:24 +0530

    Add files via upload

---
+++ greatof3.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the 1st number"))
+num2 = int(input("Enter the 2nd number"))
+num3 = int(input("Enter the 3rd number"))
+if num1>num2 and num1>num3:
+           print ("The 1st number Bigger")
+elif num2>num3 and num2>num1:
+           print ("The 2nd number bigger")
+else:
+           print ("The 3rd number bigger")
+           
